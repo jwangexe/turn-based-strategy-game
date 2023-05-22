f720875a707e380d4823d1ef5b02c6b485a3916e
Jun Wang <wjun9189@gmail.com>
2023-05-22 13:01:00 +0100

    Added system to track cursor on grid

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ RED = (255,0,0)
 GREEN = (0,255,0)
 BLUE = (0,0,255)
 GRID_GREEN = (0, 153, 51)
- 
+
 pygame.init()
  
 fps = 60
@@ -32,23 +32,36 @@ def draw_grid(square_size):
         pygame.draw.line(screen, GRID_GREEN, (x, 0), (x, gridheight), 1)
         for y in range(0, gridheight+square_size, square_size):
            pygame.draw.line(screen, GRID_GREEN, (0, y), (gridwidth, y))
-            
 
-# Game loop.
+def get_grid_pos(mousex, mousey):
+    x = mousex // squaresize
+    y = mousey // squaresize
+    if x > game_board_cols or y > game_board_rows:
+        return (-1, -1)
+    return (x, y)
+
+
+# Game loop
 
 while True:
-  screen.fill(BLACK)
+    screen.fill(BLACK)
   
-  for event in pygame.event.get():
-    if event.type == QUIT:
-      pygame.quit()
-      sys.exit()
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            pygame.quit()
+            sys.exit()
   
-  # Update.
+    # Check keypresses
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_p]:
+        mouse_x, mouse_y = pygame.mouse.get_pos()
+        print(get_grid_pos(mouse_x, mouse_y))
+  
+    # Update
   
   
-  # Draw.
-  draw_grid(squaresize)
+    # Draw
+    draw_grid(squaresize)
   
-  pygame.display.flip()
-  fpsClock.tick(fps)
+    pygame.display.flip()
+    fpsClock.tick(fps)
